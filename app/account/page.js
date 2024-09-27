1f57ea7c33ba8bf875ed492a13bfe054d42c9c02
Nataliia Hirniak <gn.natalia19@gmail.com>
2024-09-27 11:44:52 +0100

    feat: adding basic routing

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div>Hello Next!</div>;
+  return <div>Account page</div>;
 }
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div>Hello Next!</div>;
+  return <div>Cabins page</div>;
 }
